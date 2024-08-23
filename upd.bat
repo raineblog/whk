278e6f4426159aaccda56eb41645f78b377c32a6
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-08-23 18:06:20 +0800

    Update upd.bat

--- upd.bat
+++ upd.bat
@@ -1,6 +1,11 @@
+@echo off
+ipconfig /flushdns
+git config --unset http.proxy
+git config --unset https.proxy
 git config https.proxy http://127.0.0.1:10809
 git config https.proxy https://127.0.0.1:10809
-rem git config --unset http.proxy
-rem git config --unset https.proxy
-rem git submodule add https://github.com/RainPPR/material
+@REM git submodule add https://github.com/RainPPR/material
+@REM git submodule add https://github.com/RainPPR/intro docs/intro/
+echo.updating...
 git submodule update --remote
+pause
