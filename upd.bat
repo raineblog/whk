62ecbfbe680cbabb9bab7f934ff7d82cb4128596
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-08-23 15:29:48 +0800

    Update upd.bat

--- upd.bat
+++ upd.bat
@@ -3,4 +3,4 @@ git config https.proxy https://127.0.0.1:10809
 rem git config --unset http.proxy
 rem git config --unset https.proxy
 rem git submodule add https://github.com/RainPPR/material
-rem git submodule update --remote
+git submodule update --remote
