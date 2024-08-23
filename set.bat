3f12d0799cc33a2c19df0b29475ad2704d47c262
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-08-23 15:17:11 +0800

    Commit

--- set.bat
+++ set.bat
@@ -2,3 +2,4 @@ git config https.proxy http://127.0.0.1:10809
 git config https.proxy https://127.0.0.1:10809
 rem git config --unset http.proxy
 rem git config --unset https.proxy
+rem git submodule add https://github.com/RainPPR/material
