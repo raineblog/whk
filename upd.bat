f24fc6ecaab10a86e06a58c2b269f26ee8306200
RainPPR <41764766+RainPPR@users.noreply.github.com>
2024-09-16 10:24:49 +0800

    Create docs/misc/math

--- upd.bat
+++ upd.bat
@@ -1,9 +1,9 @@
 @echo off
 ipconfig /flushdns
-git config --unset http.proxy
-git config --unset https.proxy
-git config https.proxy http://127.0.0.1:10809
-git config https.proxy https://127.0.0.1:10809
+rem git config --unset http.proxy
+rem git config --unset https.proxy
+rem git config https.proxy http://127.0.0.1:10809
+rem git config https.proxy https://127.0.0.1:10809
 @REM git submodule add https://github.com/RainPPR/material
 @REM git submodule add https://github.com/RainPPR/intro docs/intro/
 echo.updating...
