03d6f8cc77f68dfec9aee0d87109a9826040d1fc
RainPPR <PPR2125773894@163.com>
2026-03-28 15:03:03 +0800

    Update generate_frontmatter.js

--- scripts/generate_frontmatter.js
+++ scripts/generate_frontmatter.js
@@ -48,7 +48,7 @@ const EXCLUDE_DIRS = ['blog', 'home', 'cultural'];
 const EXCLUDE_FILES = [path.join(DOCS_DIR, 'index.md')];
 
 const limiter = new Bottleneck({
-    minTime: 3000,
+    minTime: 5000,
     maxConcurrent: 16
 });
 
