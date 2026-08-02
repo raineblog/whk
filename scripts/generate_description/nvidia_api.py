2bda7ee181c835c833d5114fcf39082f0dfb7561
RainPPR <PPR2125773894@163.com>
2026-08-02 22:27:47 +0800

    Update nvidia_api.py

--- scripts/generate_description/nvidia_api.py
+++ scripts/generate_description/nvidia_api.py
@@ -15,7 +15,7 @@ def get_description(file: str, options):
         user_content += f"{key}：{value}\n"
 
     completion = client.chat.completions.create(
-        model="nvidia/ising-calibration-1.5-31b",
+        model="stepfun-ai/step-3.7-flash",
         messages=[
             {
                 "role": "system",
@@ -26,7 +26,7 @@ def get_description(file: str, options):
                 "content": f"{file_content}\n\n---\n\n给上面的文章，拟一个纯文本的描述 Description 字段，用于 SEO/GEO，必须是简体中文，75～100 字左右，不要输出其他任何内容。\n\n{user_content}",
             },
         ],
-        max_tokens=32768,
+        # max_tokens=32768,
         stream=True,
         # extra_body={"chat_template_kwargs": {"enable_thinking": True}},
     )
